b674898cac764ba555ab38953f6996cba8a6f21d
Mike Denisov <mike.denysov@gmail.com>
2016-04-24 02:25:52 +0300

    scene, solar panel added

---
+++ NewHome/Assets/Scripts/GameScripts/BaseModules/SolarPanelModule.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolarPanelModule : BaseModule
+{
+    [SerializeField] float _electricityProduction = 5f;
+
+    public override ModuleType ModuleType
+    {
+        get { return ModuleType.SolarPanel; }
+    }
+
+    protected override void ProductionUpdate(float deltaTime)
+    {
+//        throw new System.NotImplementedException();
+    }
+
+    protected override void ApplyResourcesConsumption(BaseResourcesBalance resources, float deltaTime)
+    {
+        resources.AddResource(BaseResourceType.Electricity, _electricityProduction * deltaTime);
+        //throw new System.NotImplementedException();
+    }
+}
